7bd54558be63fa6685ca3d39a3e6467939a99bdc
Syu7Crow <p.crow1216@gmail.com>
2026-05-27 11:39:15 +0900

    test-s068

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './lib/supabase'
 import './lib/groq'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' // 👈 追加
 import { HomePage } from './pages/HomePage'
+import { FoodPage } from './pages/FoodPage' // 👈 移動先のページをインポート（実際のファイル名に合わせてください）
+import { RecipePage } from './pages/RecipePage'
 
 function App() {
-  return <HomePage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* URLが「/」のときは HomePage を表示 */}
+        <Route path="/" element={<HomePage />} />
+        
+        {/* URLが「/foodpage」のときは FoodPage を表示 */}
+        <Route path="/FoodPage" element={<FoodPage />} />
+
+        <Route path="/RecipePage" element={<RecipePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
